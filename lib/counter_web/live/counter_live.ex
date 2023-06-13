f1511ff262ff8c00ef66fe2123281912651caf51
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2023-06-13 10:02:56 +0900

    CounterLiveにイベントを追加

--- lib/counter_web/live/counter_live.ex
+++ lib/counter_web/live/counter_live.ex
@@ -4,13 +4,33 @@ defmodule CounterWeb.CounterLive do
   def render(assigns) do
     ~H"""
     <h1>This count is：<%= @value %></h1>
-    <.button type="button">-</.button>
-    <.button type="button">+</.button>
+    <.button type="button" phx-click="dec">-</.button>
+    <.button type="button" phx-click="inc">+</.button>
     """
   end
 
   # "/counter"のルートにアクセスがあった場合まずこのmountが呼ばれる
   def mount(_params, _session, socket) do
     {:ok, assign(socket, :value, 0)}
+  end
+
+  # <.button type="button" phx-click="inc">+</.button>のボタンが押された時に呼ばれるイベント
+  def handle_event("inc", _params, socket) do
+    socket =
+      update(socket, :value, fn value ->
+        value + 1
+      end)
+
+    {:noreply, socket}
+  end
+
+  # <.button type="button" phx-click="dec">-</.button>のボタンが押された時に呼ばれるイベント
+  def handle_event("dec", _params, socket) do
+    socket =
+      update(socket, :value, fn value ->
+        value - 1
+      end)
+
+    {:noreply, socket}
   end
 end
